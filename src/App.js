2f80d9b71a6420728cbabaa407acd2e5e7ae3989
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-13 22:22:25 +0530

    feat: Calculate winner and show in the page top

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function Board() {
 
   function handleClick(index) {
     // Prevents changing X <-> Y on already marked square
-    if (squares[index]) {
+    if (squares[index] || calculateWinner(squares)) {
       return
     }
 
@@ -40,8 +40,15 @@ export default function Board() {
     setSquares(Array(9).fill(null))
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => {handleClick(0)}}/>
         <Square value={squares[1]} onSquareClick={() => {handleClick(1)}}/>
@@ -62,4 +69,24 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
